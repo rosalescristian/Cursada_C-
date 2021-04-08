b022899a78cd33bfb4fa0f38cdae09a9504e8a5a
rosalescristian <rosalescristianm@gmnail.com>
2021-04-08 19:14:58 -0300

    Ejercicio Integrador - Clase 5 20210408

---
+++ Clase 5 - 20210408/Repaso/ConsoleApp1/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Producto
+    {
+        #region Atributos
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+        #endregion
+
+        #region Constructores
+        
+        #endregion
+
+    }
+}
